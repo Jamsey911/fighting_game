f70a1a753816f81cd16b97682d50c4b56eccf826
Jamsey911 <jamesalynch911@hotmail.com>
2023-11-12 01:10:07 +0000

    Add timer function with win/lose/tie message for time coundown timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -115,7 +115,7 @@ const keys = {
     }
 }
 
-function rectangularCollision({rectangle1, rectangle2}){
+function rectangularCollision({rectangle1, rectangle2}) {
     return (
         rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x &&
         rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
@@ -123,6 +123,31 @@ function rectangularCollision({rectangle1, rectangle2}){
         rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
     )
 }
+
+let timer = 10
+function decreaseTimer(){
+    setTimeout(decreaseTimer, 1000)
+    if (timer > 0) {
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+    }
+    if (timer === 0){
+        document.querySelector('#displayText').style.display = 'flex'
+        if (player.health === enemy.health) {
+            document.querySelector('#displayText').innerHTML = 'Tie'
+            console.log('tie')
+        } else if (player.health > enemy.health) {
+                document.querySelector('#displayText').innerHTML = 'The Player Wins'
+                console.log('The Player Wins')
+        } else if (player.health < enemy.health) {
+            document.querySelector('#displayText').innerHTML = 'The Enemy Wins'
+            console.log('The Enemy Wins')
+        }
+    }
+
+}
+
+decreaseTimer()
 
 function animate() {
     window.requestAnimationFrame(animate)
@@ -178,7 +203,7 @@ function animate() {
     }
 }
 
-
+// End game Based on Health
 
 
 
