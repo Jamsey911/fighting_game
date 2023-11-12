e856f427fa8fd403cfb26da3870cce63f8b516f1
Jamsey911 <jamesalynch911@hotmail.com>
2023-11-12 00:25:36 +0000

    Add healthbar interface with html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,7 @@ class Sprite {
         }
         this.color = color
         this.isAttacking
+        this.health = 100 
     }
 
     draw() {
@@ -159,6 +160,8 @@ function animate() {
         player.isAttacking
     )   {
         player.isAttacking = false
+        enemy.health -= 20
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
         console.log('go')
     }
     if (
@@ -169,6 +172,8 @@ function animate() {
         enemy.isAttacking
     )   {
         enemy.isAttacking = false
+        player.health -= 20
+        document.querySelector('#playerHealth').style.width = player.health + '%'
         console.log('Enemy Attack')
     }
 }
