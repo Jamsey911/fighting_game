9fef1867d9959460386095dc7aca7b5da572e9ca
Jamsey911 <jamesalynch911@hotmail.com>
2023-11-15 19:27:37 +0000

    Add shop sprite with animation

--- js/script.js
+++ js/script.js
@@ -17,6 +17,17 @@ const background = new Sprite({
     imageSrc: './assets/img/background.png'
 })
 
+const shop = new Sprite({
+    position: {
+        x: 600,
+        y: 128
+    },
+    imageSrc: './assets/img/shop.png',
+    scale: 2.75,
+    framesMax: 6
+})
+
+
 
 const player = new Fighter({
     position: {
@@ -74,7 +85,7 @@ function animate() {
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
     background.update()
-    // shop.update()
+    shop.update()
     // c.fillStyle = 'rgba(255, 255, 255, 0.15)'
     // c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
